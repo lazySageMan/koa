5ae191e8eadb6076c2b5810e096b1eec86eeb207
pengyigao <lazysage@126.com>
2019-07-26 14:34:54 +0800

    处理跟新

--- router/admin/index.js
+++ router/admin/index.js
@@ -1,21 +1,24 @@
+
+// 博客后台接口的开发
 const Router = require('koa-router')
 const router = new Router()
 const { mysql } = require('../../db/mysql');
-router
-    .get('/getUser', async (ctx, next) => {
-        let con = await mysql('user').select('*')
-        ctx.body = {
-            code: 200,
-            data: con,
-            message: '恭喜你，成功！'
-        }
-    })
-    .post('/', async (ctx, next) => {
-        let con = await mysql('user').select('*')
-        ctx.body = {
-            code: 200,
-            data: con
-        }
-    })
+
+// 获取用户列表
+router.get('/getUser', async (ctx, next) => {
+    let con = await mysql('user').select('*')
+    ctx.body = {
+        code: 200,
+        data: con,
+        message: '恭喜你，成功！'
+    }
+})
+
+// 后台用户登录
+router.post('/login', async (ctx, next) => {
+    let con = await mysql('user').select('*')
+    
+})
+    
 
 module.exports = router;
